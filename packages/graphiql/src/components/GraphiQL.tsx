ba1aad17d4d3c8c0610130727f4f89f2ee99c37c
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-05-21 12:08:01 +0200

    fix tests

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -1276,7 +1276,7 @@ class GraphiQLWithContext extends React.Component<
             let maybeMultipart = Array.isArray(result) ? result : false;
             if (
               !maybeMultipart &&
-              typeof result !== 'string' &&
+              typeof result === 'object' &&
               result !== null &&
               'hasNext' in result
             ) {
